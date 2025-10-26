a8bee48215264433e5576e06a69309e2124a44c0
DAARN5 <163350434+DAARN5@users.noreply.github.com>
2025-10-26 16:04:08 +0200

    change

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,7 +1,6 @@
 import { nextServer } from './api';
 import { cookies } from 'next/headers';
-import { serverApiInstance } from './axiosInstance';
-import type { Note, Note-sHTTPResponse } from '@/types/note';
+import type { Note, NotesHTTPResponse } from '@/types/note';
 import type { User } from '@/types/user';
 import type { AxiosResponse } from 'axios';
 
@@ -23,7 +22,7 @@ export const fetchNotes = async (
   params?: { search?: string; page?: number; tag?: string }
 ): Promise<NotesHTTPResponse> => {
   const headers = await cookieHeaders();
-  const res = await serverApiInstance.get<NotesHTTPResponse>('/notes', {
+  const res = await nextServer.get<NotesHTTPResponse>('/notes', {
     params,
     headers,
   });
@@ -32,14 +31,14 @@ export const fetchNotes = async (
 
 export const fetchNoteById = async (id: string): Promise<Note> => {
   const headers = await cookieHeaders();
-  const res = await serverApiInstance.get<Note>(`/notes/${id}`, { headers });
+  const res = await nextServer.get<Note>(`/notes/${id}`, { headers });
   return res.data;
 };
 
 // ----------- USER -----------
 export const getMe = async (): Promise<User> => {
   const headers = await cookieHeaders();
-  const res = await serverApiInstance.get<User>('/users/me', { headers });
+  const res = await nextServer.get<User>('/users/me', { headers });
   return res.data;
 };
 
@@ -47,5 +46,5 @@ export const getMe = async (): Promise<User> => {
 export const checkSession = async (): Promise<AxiosResponse<User>> => {
   const headers = await cookieHeaders();
   //  повертаємо повний AxiosResponse, як вимагає завдання
-  return await serverApiInstance.get<User>('/auth/session', { headers });
+  return await nextServer.get<User>('/auth/session', { headers });
 };
