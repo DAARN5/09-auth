ebab2ad8bfe7c09a612f9b04b1d50cad237255b2
DAARN5 <163350434+DAARN5@users.noreply.github.com>
2025-10-26 11:39:29 +0200

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import css from "../components/Home/Home.module.css";
+import css from "../components/Home/Home.module.css"; 
 
 export default function Page() {
   return (
